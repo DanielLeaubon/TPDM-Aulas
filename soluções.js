acc087ff00ad1a971ad4173b2a938b7d2bcc304a
Daniel Leaubon <134809651+DanielLeaubon@users.noreply.github.com>
2024-08-09 13:57:33 -0300

    Update soluções.js

--- soluções.js
+++ soluções.js
@@ -3,3 +3,23 @@ function cumprimentar(nome) {
     return [saudacao, nome].join(", ").concat(" Seja bem vindo!")
     
 }
+const nomemes = (mes) =>{
+    const meses = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
+    return meses(--mes)
+}
+console.log(nomemes(1));
+
+const maiorOuMenor = (n1, n2) =>{
+    //if(n1 === undefined || n2 == undefined) return "Valores não desfinidos"
+    if(n1 === NaN || n2 === NaN) return "valor não numérico"
+    if(n1 > n2){
+        return `O valor ${n1} é maior que o valor ${n2}`
+    }
+    else if (n1 < n2){
+        return `O valor de ${n1} é manor que o valor de ${n2}`
+    }
+    else {
+        return `O valor de ${n1} é manor que o valor de ${n2}`
+    }
+}
+console.log(maiorOuMenor(1,3));
