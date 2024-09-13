25318c3a5e40a468312fe15498e97d4a35e445b3
Daniel Leaubon <134809651+DanielLeaubon@users.noreply.github.com>
2024-09-13 14:25:33 -0300

    Update atividade-desafio.js

--- atividade-desafio.js
+++ atividade-desafio.js
@@ -3,6 +3,7 @@ const numerosDuplicados = numeros.map(number => number * 2)
 console.log(numerosDuplicados);
 
 const numeros2 = [1,2,3,4,5,6,7,8,9]
+const dobro = e => e*2
 const numerosDuplicados2 = numeros2.map(number => (number * 2).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' }));
 console.log(numerosDuplicados2);
 
@@ -15,5 +16,7 @@ let compras = [
 ]
 const paraObjeto = json =>JSON.parse(json)
 const precoApenas = produto => produto.preço
-const resultado2 = compras.map(paraObjeto).map(precoApenas)
+const dinheiros = e => `R$${parseFloat(e).toFixed(2).replace('.',',')}`
+//const resultado2 = compras.map(paraObjeto).map(precoApenas).map(dobro).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+const resultado2 = compras.map(paraObjeto).map(precoApenas).map(dobro).map(dinheiros)
 console.log(resultado2);
