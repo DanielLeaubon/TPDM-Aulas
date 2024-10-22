48903cea1e85fbbf5a5cb292043d7db8e6d3c101
Daniel Leaubon <134809651+DanielLeaubon@users.noreply.github.com>
2024-10-22 12:05:32 -0300

    Update funcionarios.js

--- funcionarios.js
+++ funcionarios.js
@@ -7,7 +7,12 @@ const mulheres = f => f.genero === 'F'
 const menorSalario = (func, funcAtual) =>{
     return func.salario < funcAtual.salario ? func : funcAtual
 }
+const alemaes = f => f.pais === 'Alemanha'
+
+const primeiraLetraA = f => f.nome[0] === 'A' 
 
 const resultado = urlLocal.filter(brasileiros).filter(mulheres).reduce(menorSalario)
+
+const resultado2 = urlLocal.filter(alemaes).map()
 
 console.log(resultado);
