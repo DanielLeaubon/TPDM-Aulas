79cab61d52a5de6ebe92d224581d24d09ea8a417
DanielLeaubon <134809651+DanielLeaubon@users.noreply.github.com>
2024-10-29 11:32:44 -0300

    Atividade

--- funcionarios.js
+++ funcionarios.js
@@ -1,18 +1,19 @@
-const urlLocal = require('./funcionarios.json')
+const urlLocal = require('./funcionarios.json');
 
-const brasileiros = f => f.pais === 'Brasil'
+const brasileiros = f => f.pais === 'Brasil';
+const mulheres = f => f.genero === 'F';
+const menorSalario = (func, funcAtual) => func.salario < funcAtual.salario ? func : funcAtual;
+const alemaes = f => f.pais === 'Alemanha';
+const primeiraLetraA = f => f.nome[0] === 'A';
+const nomes = f => f.nome
+// Encontrar a funcionária brasileira com o menor salário
+const resultado = urlLocal.filter(brasileiros).filter(mulheres).reduce(menorSalario);
+console.log('Funcionária Brasileira com Menor Salário:', resultado);
 
-const mulheres = f => f.genero === 'F'
+// Extrair nomes dos funcionários alemães
+const nomesAlemaes = urlLocal.filter(alemaes).filter(primeiraLetraA).map(nomes);
+console.log('Nomes dos Funcionários Alemães:', nomesAlemaes);
 
-const menorSalario = (func, funcAtual) =>{
-    return func.salario < funcAtual.salario ? func : funcAtual
-}
-const alemaes = f => f.pais === 'Alemanha'
-
-const primeiraLetraA = f => f.nome[0] === 'A' 
-
-const resultado = urlLocal.filter(brasileiros).filter(mulheres).reduce(menorSalario)
-
-const resultado2 = urlLocal.filter(alemaes).map()
-
-console.log(resultado);
+// Calcular a soma dos salários dos funcionários alemães
+const somaSalariosAlemaes = urlLocal.filter(alemaes).reduce((total, func) => total + func.salario, 0);
+console.log(`Soma dos Salários dos Funcionários Alemães: R$${somaSalariosAlemaes}`);
